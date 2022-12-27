798a18bea8af454f6857324770d53ebd9001bc0a
Aiman Fariz <99475959+AimanFariz@users.noreply.github.com>
2022-12-28 00:09:56 +0800

    update confiq

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary:'#64748b',
+        dark: '#0f172a',
+      },
+      screens:{
+        '2xl': '1320px',
+      }
+    },
   },
   plugins: [],
 }
